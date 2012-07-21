40153da1a01569e9472afea7bdc8486419ff3292
Max Masnick <max@masnick.me>
2012-07-21 01:06:28 -0600

    Stripe fee constant fix

--- config/initializers/cornrows.rb
+++ config/initializers/cornrows.rb
@@ -1,5 +1,5 @@
 Stripe.api_key = ENV['STRIPE_API'] || "1lMkTGRDtLPBL50Q3j6UaIFQgCN5auzc"
 STRIPE_PUBLIC_KEY = ENV['STRIPE_API_PUBLIC'] || "pk_6a4NM4z8RKKGHWPlOHQfhlZlKtakp"
-STRIPE_FEE = ENV['STRIPE_FEE'].to_f || 0.029 # Stripe per-transaction percentage as a decimal (ignore the $0.30)
+STRIPE_FEE = ENV['STRIPE_FEE'] ? ENV['STRIPE_FEE'] : 0.029 # Stripe per-transaction percentage as a decimal (ignore the $0.30)
 BUSINESS_NAME = ENV['BUSINESS_NAME'] || "Cassava Labs, LLC"
 SUPPORT_EMAIL = ENV['SUPPORT_EMAIL'] || "team@cassavalabs.com"
