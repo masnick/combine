c759c9034abdd154ea3a2a3a806f2e563461603f
Max Masnick <max@masnick.me>
2012-07-21 00:26:55 -0600

    fix bad migration

--- db/migrate/20120721052939_add_stripe_charge_hash_to_invoices.rb
+++ db/migrate/20120721052939_add_stripe_charge_hash_to_invoices.rb
@@ -1,10 +1,10 @@
 class AddStripeChargeHashToInvoices < ActiveRecord::Migration
   def up
     add_column :invoices, :stripe_charge_hash, :string
-    remove_column :invoices, :paid, :boolean
+    remove_column :invoices, :paid
   end
   def down
-    remove_column :invoices, :stripe_charge_hash, :string
+    remove_column :invoices, :stripe_charge_hash
     add_column :invoices, :paid, :boolean
   end
 end
