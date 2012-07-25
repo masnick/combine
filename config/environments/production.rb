00e2e0caa67f72c539ee7a2a9ee7ca1b6c174e93
Max Masnick <max@masnick.me>
2012-07-24 23:52:56 -0700

    Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,20 @@ Combine::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => ENV['APP_URL'] }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+       :authentication => :plain,
+       :address => "smtp.mailgun.org",
+       :port => 587,
+       :domain => "pintrips.mailgun.org",
+       :user_name => ENV['MAILGUN_MAILBOX'],
+       :password => ENV['MAILGUN_PASSWORD']
+  }
 end
